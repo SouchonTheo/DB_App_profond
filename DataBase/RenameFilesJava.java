eff22c96589dd82a1d5c3f7e7c27a96694e43f16
Sebeck10 <souchontheo24@gmail.com>
2022-03-24 17:42:14 +0100

    testJava

--- DataBase/RenameFilesJava.java
+++ DataBase/RenameFilesJava.java
@@ -4,36 +4,33 @@ import java.io.IOException;
 public class RenameFilesJava {
  
     public static void main(String[] argv) throws IOException {
-
-        File dossier = new File("c:\\test");
+        String nomDossier = "test"; //mettre le nom du dossier
+        int premierNum = 0; //mettre le numéro de départ
+        File dossier = new File(nomDossier); 
         File[] liste = dossier.listFiles();
 
         for (int i = 0; i < liste.length; i++) {
 
-            if (liste[i].isFile()) { // si c'est un fichier
-             
-             //récupérer le fichier en cours
-             File f = new File("c:\\test\\"+liste[i].getName()); 
-             String extension = "";
-             String ancien = liste[i].getName();
-             
-             //vérifier que l'extension est txt
-             int index = ancien.lastIndexOf('.');
-             if (index > 0) {
-                 extension = ancien.substring(index+1);
-             }
-             if(extension.equals("txt")){
-                 String nouveau = "f"+i+".txt";
-                 f.renameTo(new File("c:\\test\\"+nouveau));
-                 
-                 System.out.println(ancien+" renommé => "+nouveau);
-             }
-             else{
-              System.out.println(ancien+" n'est pas un fichier txt");
-             }
+            if (liste[i].isFile()) { 
+                File f = new File(nomDossier+liste[i].getName()); 
+                String extension = "";
+                String ancien = liste[i].getName();
+                int index = ancien.lastIndexOf('.');
+                if (index > 0) {
+                    extension = ancien.substring(index+1);
+                }
+                if(extension.equals("png")){
+                    String nouveau = premierNum+".png";
+                    boolean k = f.renameTo(new File(nouveau));
+                    System.out.println(k);
+                    System.out.println(ancien+" renommé => "+nouveau);
+                }
+                else{
+                System.out.println(ancien+" n'est pas un fichier png");
+                }
             }
+            premierNum++;
         }
-
         System.out.println("Opération terminée!");
     }
 }
